6947758722f20b4008ace87c243b3e48ef9f6b6b
eunhee.choi <eunhee.choi@kurlycorp.com>
2020-12-16 16:30:19 +0900

    refactor: inputView scanner 상수처리

--- src/main/java/race/InputView.java
+++ src/main/java/race/InputView.java
@@ -3,14 +3,13 @@ package race;
 import java.util.Scanner;
 
 public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
     public int inputCarCount() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("자동차 대수는 몇 대 인가요? ");
         return scanner.nextInt();
     }
 
     public int inputRaceCount() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("시도할 회수는 몇 회 인가요? ");
         return scanner.nextInt();
     }
